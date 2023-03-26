cccfb3f3e5e88391ef894ea4cca9cae628132dd7
ICe1 <98827759+ICe1BotMaker@users.noreply.github.com>
2023-03-26 16:03:19 +0900

    Add files via upload

--- word-analyzer.ts
+++ word-analyzer.ts
@@ -46,32 +46,32 @@ const wordAnalyzer = {
             if (endText !== undefined && !line.endsWith(endText))
                 throw new SyntaxError(`line: ${idx}\nYou must add '${endText}' at the end of a sentence.`);
                 
-            words.forEach((wordObj: any) => {
+            words.forEach((wordObj: any, wordIdx: number) => {
                 const grammarReg = new RegExp(wordObj.option.grammer.replace(/\(\|\<[a-zA-Z]+\>\|\)/g, `(\\w+)`));
-                const type = wordObj.option.grammer.match(/\(\|\<[a-zA-Z]+\>\|\)/g);
 
-                if (!grammarReg.test(line))
-                    throw new SyntaxError(`line: ${idx}\nThe grammar is incorrect.`);
+                const typeMatch = wordObj.option.grammer.match(/\(\|\<[a-zA-Z]+\>\|\)/g);
+                let types: string[] = [];
+                typeMatch.forEach((e: any) => types = [...types, typeof eval(String(e.match(/(\w+)/)?.[0]))()]);
+                
+                if (grammarReg.test(line)) {
+                    // console.log(types);
+                    console.log(line.match(grammarReg)?.[2]);
+                
+                    // // result
+                    // if (wordObj.option.type === `print`) {
+                    //     console.log(type2);
+                    // }
+    
+                    // if (wordObj.option.type === `variable`) {
+                    //     console.log(type1, type2);
+                    // }
+                }
 
-                // type.forEach((match: string) => {
-                //     const type1 = typeof eval(String(match.match(/[a-zA-Z]+/g)?.[0]))();
+                // if (!grammarReg.test(line))
+                //     throw new SyntaxError(`line: ${idx}\nThe grammar is incorrect.`);
 
-                //     console.log(type1);
-                // });
-
-                // // value type error
                 // if (typeof type1 !== typeof type2)
                 //     throw new TypeError(`line: ${idx}\nInserted value is not in type '${type1}'`);
-                
-                
-                // // result
-                // if (wordObj.option.type === `print`) {
-                //     console.log(type2);
-                // }
-
-                // if (wordObj.option.type === `variable`) {
-                //     console.log(type1, type2);
-                // }
             });
         });
     }
@@ -93,4 +93,9 @@ wordAnalyzer.insertWord(`@var`, {
     delimiter: `:`
 });
 
-wordAnalyzer.analyzeGrammar(`@print 'asdf';`);
+wordAnalyzer.analyzeGrammar(
+    `
+    @print 'asdf';
+    @var test: 1;
+    `
+);
